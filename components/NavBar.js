a1d1c4311143c356b220f9ccde88e46615fb493c
frank campos <frankcampos85@gmail.com>
2024-02-17 21:54:37 -0600

    Added a 'Add A Learning Path' button to the Home component

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,26 +1,37 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
 import Link from 'next/link';
+import React from 'react';
 import {
-  Navbar, Container, Nav, Button,
+  Navbar, Container, Nav, Button, Image,
 } from 'react-bootstrap';
 import { signOut } from '../utils/auth';
 
 export default function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Link passHref href="/">
-          <Navbar.Brand>CHANGE ME</Navbar.Brand>
-        </Link>
+    <Navbar collapseOnSelect expand="lg" bg="black" variant="dark">
+      {' '}
+      {/* Change the bg and variant props here */}
+      <Container fluid>
+        <Nav>
+          <Image src="/thinkthrive.png" alt="No Simple Books" style={{ height: '40px', backgroundColor: 'transparent' }} />
+          <Link passHref href="/">
+            <Navbar.Brand style={{ marginLeft: '10px' }}>ThinkThrive</Navbar.Brand>
+          </Link>
+        </Nav>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
             <Link passHref href="/">
-              <Nav.Link>Home</Nav.Link>
+              <Nav.Link>Learning Paths</Nav.Link>
             </Link>
-            <Button variant="danger" onClick={signOut}>Sign Out</Button>
+            <Link passHref href="./paths">
+              <Nav.Link>My Paths</Nav.Link>
+            </Link>
+          </Nav>
+          <Nav style={{ alignContent: 'flex-end' }}>
+            <Button variant="dark" onClick={signOut}>
+              Sign Out
+            </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,15 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
-import { useAuth } from '../utils/context/authContext';
+import Link from 'next/link';
 
 function Home() {
-  const { user } = useAuth();
-
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
+    <div className="text-center my-4">
+      <Link href="/new" passHref>
+        <Button variant="dark" style={{ margin: '0 0 10px' }}>
+          Add A Learning Path
+        </Button>
+      </Link>
     </div>
   );
 }

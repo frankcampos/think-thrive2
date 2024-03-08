a72390a9ad1f42014314dc4fc06715bff0ed56d7
frank campos <frankcampos85@gmail.com>
2024-03-08 00:30:43 -0600

    Add Procedural Card form modal and button

--- pages/conceptual-knowledge/[firebaseKey].js
+++ pages/conceptual-knowledge/[firebaseKey].js
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { Button } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 import ConceptualCard from '../../components/conceptualCard';
+import ProceduralCardFormModal from '../../components/forms/proceduralCardFormModal';
 import { useAuth } from '../../utils/context/authContext';
 import { getConceptualKnowledgeByPathId } from '../../api/conceptualknowledgeData';
 import { getSinglePath } from '../../api/pathsData';
@@ -13,6 +14,7 @@ function ConceptualKnowledgePage() {
   const { firebaseKey } = router.query;
   const [ConceptualKnowledgeCards, setConceptualKnowledgeCards] = useState([]);
   const [pathId, setPathId] = useState('');
+  const [showModal, setShowModal] = useState(false);
   const { user } = useAuth();
 
   const getAllTheConceptualKnowledge = () => {
@@ -25,14 +27,28 @@ function ConceptualKnowledgePage() {
   useEffect(() => {
     getAllTheConceptualKnowledge();
   }, []);
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  const handleModalOpen = () => {
+    setShowModal(true);
+  };
+
   return (
     <div className="text-center my-4">
       {pathId === user.uid && (
-      <Link href={`/conceptual-knowledge/new/${firebaseKey}`} passHref>
-        <Button variant="dark" style={{ margin: '0 0 10px' }}>
-          Add A Conceptual Card
-        </Button>
-      </Link>
+        <div>
+          <Link href={`/conceptual-knowledge/new/${firebaseKey}`} passHref>
+            <Button variant="dark" style={{ margin: '0 0 10px' }}>
+              Add A Conceptual Card
+            </Button>
+          </Link>
+          <Button variant="dark" style={{ margin: '0 10px 10px' }} onClick={handleModalOpen}>
+            Add A Procedural Card
+          </Button>
+          <ProceduralCardFormModal show={showModal} onHide={handleModalClose} />
+        </div>
       )}
       <div className="d-flex flex-wrap justify-content-center">
         {ConceptualKnowledgeCards.map((card) => (

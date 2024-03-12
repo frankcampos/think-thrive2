2631cd1cdc86d9918c926b1aba03af54f5587021
frank campos <frankcampos85@gmail.com>
2024-03-11 21:33:57 -0500

    Add deleteProceduralKnowledge function and update ProceduralCardFormModal component

--- components/proceduralCard.js
+++ components/proceduralCard.js
@@ -2,10 +2,27 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import ProceduralCardFormModal from './forms/proceduralCardFormModal';
+import { deleteProceduralKnowledge } from '../api/proceduralknowledgeData';
 import { useAuth } from '../utils/context/authContext';
 
 function ProceduralCard({ proceduralCard, onUpdate, userID }) {
   const { user } = useAuth();
+  const [showModal, setShowModal] = useState(false);
+
+  const deletethisProceduralCard = () => {
+    if (window.confirm(`Are you sure you want to delete this ${proceduralCard.title}?`)) deleteProceduralKnowledge(proceduralCard.firebaseKey).then(onUpdate());
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  const handleModalOpen = () => {
+    setShowModal(true);
+  };
+
   console.warn('onUpdate', onUpdate);
   return (
     <Card
@@ -34,16 +51,27 @@ function ProceduralCard({ proceduralCard, onUpdate, userID }) {
         </Link>
         {user.uid === userID && (
         <>
-          <Link href="./" passHref>
-            <Button variant="dark" style={{ marginRight: '10px' }}>
-              Edit
-            </Button>
-          </Link>
-          <Button variant="dark" onClick={console.warn('hi')} style={{ marginRight: '10px' }}>
+          <Button variant="dark" style={{ marginRight: '10px' }} onClick={handleModalOpen}>
+            Edit
+          </Button>
+          <ProceduralCardFormModal
+            show={showModal}
+            onHide={handleModalClose}
+            pathId={proceduralCard.pathId}
+            onUpdate={onUpdate}
+            objProceduralCard={proceduralCard}
+          />
+          <Button variant="dark" onClick={deletethisProceduralCard} style={{ marginRight: '10px' }}>
             Delete
           </Button>
         </>
         )}
+        <Card.Text style={{
+          background: 'grey', borderRadius: '5px', marginTop: '20px', padding: '5px', width: 'auto', color: 'blue',
+        }}
+        >
+          Procedural Card
+        </Card.Text>
       </Card.Body>
 
     </Card>
@@ -55,6 +83,8 @@ ProceduralCard.propTypes = {
   proceduralCard: PropTypes.shape({
     title: PropTypes.string.isRequired,
     picture: PropTypes.string.isRequired,
+    firebaseKey: PropTypes.string.isRequired,
+    pathId: PropTypes.string.isRequired,
   }).isRequired,
   onUpdate: PropTypes.func.isRequired,
   userID: PropTypes.string.isRequired,
